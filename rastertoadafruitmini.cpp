ccc2853bfcfbbdb2de9df2cc3b6028a67d6df206
Edward Rosten <ed@edwardrosten.com>
2019-12-21 21:37:39 +0000

    First full version, untested

--- rastertoadafruitmini.cpp
+++ rastertoadafruitmini.cpp
@@ -7,11 +7,19 @@
 #include <cmath>
 #include <algorithm>
 
+#include <signal.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
+
 using std::clog;
+using std::cerr;
 using std::cout;
 using std::endl;
+using std::min;
+using std::max;
 using std::vector;
 using std::array;
+using std::string;
 
 
 constexpr unsigned char ESC = 0x1b;
@@ -43,6 +51,19 @@ void rasterheader(uint16_t xsize, uint16_t ysize)
 	cout << GS << 'v' << '0' << '\0' << binary((xsize+7)/8) << binary(ysize);
 }
 
+void feed_lines(int n){
+	for(;n >0 ; n-=255)
+		cout << ESC << 'J' << static_cast<unsigned char>(min(255, n));
+}
+
+void feed_mm(int mm){
+	feed_lines(mm*8);
+}
+
+void horizontal_rule(int w=384){
+	rasterheader(w, 1);
+	cout << string('\xff', (w+7)/8);
+}
 
 void set_heating_time(int time_factor){
 	// Page 47 of the manual
@@ -62,21 +83,67 @@ double degamma(int p){
 	return pow(p/255., 1/2.2);
 }
 
-int main(){
 
-	cups_raster_t *ras = cupsRasterOpen(0, CUPS_RASTER_READ);
+volatile sig_atomic_t cancel_job = 0;
+
+
+
+int main(int argc, char** argv){
+
+	if(argc !=6 && argc != 7){
+		cerr << "ERROR: " << argv[0] << " job-id user title copies options [file]\n";
+		exit(1);
+	}
+
+	int input_fd = 0;
+	if(argc == 7){
+		input_fd = open(argv[6], O_RDONLY);
+		if(input_fd == -1){
+			cerr << "ERROR: could not open file " << argv[6] << ": " << strerror(errno) << endl;
+			exit(1);
+		}
+	}
+	
+	//As recommended by the CUPS documentation https://www.cups.org/doc/api-filter.html#SIGNALS
+	signal(SIGPIPE, SIG_IGN);
+	signal(SIGTERM, [](int){ cancel_job = 1;});
+
+	cups_raster_t *ras = cupsRasterOpen(input_fd, CUPS_RASTER_READ);
 	cups_page_header2_t header;
 	int page = 0;
 
+	printerInitialise();
+
+	int feed_between_pages_mm = 0;
+	int mark_page_boundary = 0;
+	int eject_after_print_mm = 0;
+	int auto_crop = 0;
+	int enhance_resolution = 0;
+
 	while (cupsRasterReadHeader2(ras, &header))
 	{
-		/* setup this page */
-		page ++;
+		page++;
+		//Write out information to CUPS 
 		clog << "PAGE: " << page << " " << header.NumCopies << "\n";
-		clog << "BPP: " << header.cupsBitsPerPixel << endl;
-		clog << "BitsPerColor: " << header.cupsBitsPerColor << endl;
-		clog << "Width: " << header.cupsWidth << endl;
-		clog << "Height: " << header.cupsHeight << endl;
+		clog << "DEBUG: bitsperpixel " << header.cupsBitsPerPixel << endl;
+		clog << "DEBUG: BitsPerColor " << header.cupsBitsPerColor << endl;
+		clog << "DEBUG: Width " << header.cupsWidth << endl;
+		clog << "DEBUG: Height" << header.cupsHeight << endl;
+
+
+		//Read the configuration parameters
+		feed_between_pages_mm = header.cupsInteger[0];
+		mark_page_boundary = header.cupsInteger[1];
+		eject_after_print_mm = header.cupsInteger[2];
+		auto_crop = header.cupsInteger[3];
+		enhance_resolution = header.cupsInteger[4];
+
+		if(page > 1)
+			feed_mm(feed_between_pages_mm);
+
+		//Avoid double marking the boundary if there's no feed
+		if(mark_page_boundary && (feed_between_pages_mm > 0 || page == 1))
+			horizontal_rule();
 
 		// Input data buffer for one line
 		vector<unsigned char> buffer(header.cupsBytesPerLine);
@@ -84,16 +151,30 @@ int main(){
 		//Error diffusion data
 		vector<vector<double>> errors(diffusion_coefficients.size(), vector<double>(buffer.size(), 0.0));
 
-		clog << "Line bytes: " << buffer.size() << endl;
-		printerInitialise();
+		clog << "DEBUG: Line bytes " << buffer.size() << endl;
 
 		/* read raster data */
+		unsigned int n_blank=0;
 		for (unsigned int y = 0; y < header.cupsHeight; y ++)
 		{
-			if (cupsRasterReadPixels(ras, buffer.data(), header.cupsBytesPerLine) == 0)
-				break;
+			if(cancel_job){
+				goto finish;
+			}
 
+			if(cupsRasterReadPixels(ras, buffer.data(), header.cupsBytesPerLine) == 0)
+				break;
 			
+			if(find_if(buffer.begin(), buffer.end(), [](auto c){return c!=255;}) == buffer.end()){
+				n_blank++;
+				continue;
+			}
+			
+			//Auto crop means automatically remove white from the beginning and end of the page
+			if(!auto_crop || n_blank != y)
+				feed_lines(n_blank);
+
+			n_blank = 0;
+
 			//Estimate the lowest value pixel in the row
 			double low_val=1.0;
 			for(int i=0; i < (int)buffer.size(); i++)
@@ -101,6 +182,9 @@ int main(){
 			//Add some headroom otherwise black areas bleed because it can't go
 			//dark enough
 			low_val*=0.99;
+
+			if(!enhance_resolution)
+				low_val = 0;
 
 			//Set the darkness based on the darkest pixel we want
 
@@ -141,16 +225,26 @@ int main(){
 			}
 			if(bits)
 				cout << current;
+		
 			
+
 			//Roll the buffer round.
 			std::rotate(errors.begin(), errors.begin()+1, errors.end());
 			for(auto& p:errors.back())
 				p=0;
-	
 		}
 
-		/* finish this page */
+		//Finish page
+		if(!auto_crop)
+			feed_lines(n_blank);
+
+		if(mark_page_boundary)
+			horizontal_rule();
 	}
-	cout << "\n\n\n\n\n\n";
+
+	finish:
+
+	feed_mm(eject_after_print_mm);
+	printerInitialise();
 	cupsRasterClose(ras);
 }
